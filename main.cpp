8a102c3a7f4b1cdc9f9fdd50b8c27f081b2d3523
Sherman Yan <syan1689@gmail.com>
2022-08-31 14:37:53 -0700

    sfml setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(600, 600), "My window");
+    sf::CircleShape shape(300.f);
+    shape.setFillColor(sf::Color(100, 250, 50));
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
